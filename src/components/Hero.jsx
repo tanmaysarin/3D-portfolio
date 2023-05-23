a18bbebc83277115b4257c529e5488d12ecb4723
Tanmay Sarin <tanmaysarin@gmail.com>
2023-05-23 02:27:53 -0500

    adding the scroll-down motion gif and making the canvas small screen compatible

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,39 @@ const Hero = () => {
       </div>
       {/** Generating the Left->Right UI for the section */}
 
+      {/** Load the computer canvas */}
       <ComputersCanvas />
+      {/** Load the computer canvas */}
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full
+      flex justify-center items-center'>
+
+        {/** Starting the #About Section */}
+        <a href='#about'>
+
+          {/** Making the Scrolling Gif Like motion box */}
+          <div className='w-[35px] h-[64px] rounded-3xl border-4
+          border-secondary flex justify-center items-start p-2'>
+
+            <motion.dev 
+              animate= {{
+                y: [0, 24, 0] // Motions is only possible on the y-axis in a 24px area
+              }}
+              transition= {{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className = 'w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+
+          </div>
+          {/** Making the Scrolling Gif Like motion box */}
+
+        </a>
+        {/** Starting the #About Section */}
+
+      </div>
 
     </section>
     // The Top->Down UI for the screen
