b52538d3caa43bd85cbacd041390f1b3df2b1a11
Tanmay Sarin <tanmaysarin@gmail.com>
2023-05-30 02:35:14 -0500

    completing the texh section and adding the ball canvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { BallCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { technologies } from '../constants' // the technologies
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    // Adding a div to contain all the balls
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+
+      {/** Going through the list of all the technologies */}
+      {technologies.map((technology) => (
+        
+        // Container for a single Ball
+        <div className='w-28 h-28' key={technology.name}>
+
+          {/** Adding the Canvas for the Ball */}
+          <BallCanvas icon = {technology.icon} />
+
+        </div>
+        // Container for a single Ball
+
+      ))}
+      {/** Going through the list of all the technologies */}
+
+    </div>
+    // Adding a div to contain all the balls
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, '')
